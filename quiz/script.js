2335839fc89bf86850854f212a15fddda0924ad4
udaya <udayaar1432@gmail.com>
2023-08-29 17:53:56 +0530

    add timer count

--- quiz/script.js
+++ quiz/script.js
@@ -2,17 +2,18 @@ const questions = [
     {
         question: "What is the capital of France?",
         options: ["Paris", "Berlin", "London", "Madrid"],
-        correctAnswer: "Paris"
+        correctAnswer: "Paris",
+        actualkey: "0"
     },
     {
         question: "Which planet is known as the Red Planet?",
         options: ["Earth", "Mars", "Jupiter", "Venus"],
-        correctAnswer: "Mars"
+        correctAnswer: "Mars",
     },
     {
         question: "What is the largest mammal?",
         options: ["Elephant", "Giraffe", "Blue Whale", "Hippopotamus"],
-        correctAnswer: "Blue Whale"
+        correctAnswer: "Blue Whale",
     }
 ];
 
@@ -20,45 +21,55 @@ let currentQuestionIndex = 0;
 const questionElement = document.getElementById('question');
 const optionsElement = document.getElementById('options');
 const prevButton = document.getElementById('prevButton');
-const nextButton = document.getElementById('nextButton');
 const addQuestionForm = document.getElementById('addQuestionForm');
 
-addQuestionForm.addEventListener('submit', function (event) {
-    event.preventDefault();
 
-    const newQuestion = document.getElementById('newQuestion').value;
-    const newOptions = document.getElementById('newOptions').value.split(',');
-    const newCorrectAnswer = document.getElementById('newCorrectAnswer').value;
+const timeLeftDisplay = document.getElementById('timeLeftDisplay');
+const questionTime = 30;
+let timeLeft = questionTime;
+let timerInterval;
 
-    if (newQuestion && newOptions.length && newCorrectAnswer) {
-        const newQuestionObj = {
-            question: newQuestion,
-            options: newOptions.map(option => option.trim()),
-            correctAnswer: newCorrectAnswer.trim()
-        };
+function startTimer() {
+    timeLeft = questionTime;
+    timeLeftDisplay.textContent = timeLeft;
 
-        questions.push(newQuestionObj);
-        alert('New question added successfully!');
-        showQuestion();
-        // Clear form fields
-        document.getElementById('newQuestion').value = '';
-        document.getElementById('newOptions').value = '';
-        document.getElementById('newCorrectAnswer').value = '';
-    } else {
-        alert('Please fill in all fields.');
-    }
+    clearInterval(timerInterval);
+    timerInterval = setInterval(() => {
+        timeLeft--;
+        timeLeftDisplay.textContent = timeLeft;
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            checkAnswer();
+            showNextQuestion();
+        }
+    }, 1000);
+}
+
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    body.classList.toggle('light');
 });
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    body.classList.add('dark');
+} else {
+    body.classList.add('light');
+}
 
 function showQuestion() {
     const currentQuestion = questions[currentQuestionIndex];
     questionElement.textContent = currentQuestion.question;
     optionsElement.innerHTML = "";
 
+    console.log(currentQuestion.actualKey);
     currentQuestion.options.forEach((option, index) => {
         const radioBtn = document.createElement('input');
         radioBtn.type = 'radio';
         radioBtn.name = 'option';
         radioBtn.value = option;
+        radioBtn.checked = option == currentQuestion.actualKey
         optionsElement.appendChild(radioBtn);
 
         const label = document.createElement('label');
@@ -74,12 +85,10 @@ function checkAnswer() {
     if (!selectedOption) {
         return false;
     }
-
+    questions[currentQuestionIndex].actualKey = selectedOption.value;
     const currentQuestion = questions[currentQuestionIndex];
     return selectedOption.value === currentQuestion.correctAnswer;
 }
-
-
 
 nextButton.addEventListener('click', () => {
     if (checkAnswer()) {
@@ -97,6 +106,17 @@ nextButton.addEventListener('click', () => {
         nextButton.disabled = true;
     }
 
+    if (currentQuestionIndex < questions.length) {
+        startTimer();
+        showQuestion();
+        questionSelector.value = currentQuestionIndex;
+        prevButton.disabled = false;
+    } else {
+        alert('Quiz is over!');
+        nextButton.disabled = true;
+        stopTimer();
+    }
+
 });
 prevButton.addEventListener('click', () => {
     currentQuestionIndex--;
@@ -106,59 +126,14 @@ prevButton.addEventListener('click', () => {
     else {
         currentQuestionIndex[0].disabled = true;
     }
+
+    if (currentQuestionIndex >= 0) {
+        showQuestion();
+        questionSelector.value = currentQuestionIndex;
+        nextButton.disabled = false;
+    } else {
+        currentQuestionIndex = 0;
+    }
 })
 
-
-
-
-
 showQuestion();
-
-
-
-
-// function Click() {
-//     let c = 0;
-//     let q1 = document.quiz.question1.value;
-//     let q2 = document.quiz.question2.value;
-//     let q3 = document.quiz.question3.value;
-//     let q4 = document.quiz.question4.value;
-//     let q5 = document.quiz.question5.value;
-//     let q6 = document.quiz.question6.value;
-//     let q7 = document.quiz.question7.value;
-//     let q8 = document.quiz.question8.value;
-//     let q9 = document.quiz.question9.value;
-//     let q10 = document.quiz.question10.value;
-//     if (q1 == "Guido van Rossum") {
-//         c++
-//     }
-//     if (q2 == "None of the mentioned") {
-//         c++
-//     }
-//     if (q3 == "7") {
-//         c++;
-//     }
-//     if (q4 == "def") {
-//         c++;
-//     }
-//     if (q5 == "#") {
-//         c++;
-//     }
-//     if (q6 == "DECIMAL") {
-//         c++;
-//     }
-//     if (q7 == "TRUNCATE") {
-//         c++;
-//     }
-//     if (q8 == "Only 1") {
-//         c++;
-
-//     }
-//     if (q9 == "COMPUTE") {
-//         c++;
-//     }
-//     if (q10 == "ALTER") {
-//         c++;
-//     }
-//     document.write("<h1>You scored " + c + " out of 10</h1>");
-// }
