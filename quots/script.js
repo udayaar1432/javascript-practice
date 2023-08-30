43479b74b3f36219414f78e1ae6beeffa2718875
udaya <udayaar1432@gmail.com>
2023-08-30 16:10:55 +0530

    random quotes

---
+++ quots/script.js
@@ -0,0 +1,28 @@
+
+const quotes = [
+    {
+        quote: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs",
+        photo: ""
+    },
+    {
+        quote: "In three words I can sum up everything I've learned about life: it goes on.",
+        author: "Robert Frost",
+        photo: ""
+    },
+
+];
+
+const quoteElement = document.getElementById('quote');
+const authorNameElement = document.getElementById('authorName');
+const authorPhotoElement = document.getElementById('authorPhoto');
+const generateButton = document.getElementById('generateButton');
+
+generateButton.addEventListener('click', () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+
+    quoteElement.textContent = randomQuote.quote;
+    authorNameElement.textContent = `- ${randomQuote.author}`;
+    authorPhotoElement.src = randomQuote.photo;
+});
